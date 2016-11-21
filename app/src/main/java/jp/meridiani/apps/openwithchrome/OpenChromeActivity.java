dc26dc7aff5341651c0a240830e621a6ff8e345f
hashiz <hashiz@meridiani.jp>
2016-11-21 18:14:06 +0900

    a

---
+++ app/src/main/java/jp/meridiani/apps/openwithchrome/OpenChromeActivity.java
@@ -0,0 +1,13 @@
+package jp.meridiani.apps.openwithchrome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OpenChromeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_chrome);
+    }
+}
