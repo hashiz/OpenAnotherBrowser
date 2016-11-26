cd33190b4357cd33b8efd4d88cae0eb62fe559fa
hashiz <hashiz@meridiani.jp>
2016-11-26 17:00:58 +0900

    It working!

---
+++ app/src/main/java/jp/meridiani/apps/openwithchrome/BrowserList.java
@@ -0,0 +1,49 @@
+package jp.meridiani.apps.openwithchrome;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by kjcs_hashi on 2016/11/26.
+ */
+
+public class BrowserList {
+    private static BrowserList mInstance = null;
+    private SharedPreferences mStore = null;
+
+    protected BrowserList(Context context) {
+        mStore = context.getSharedPreferences("BrowserList", Context.MODE_PRIVATE);
+    }
+
+    public static synchronized BrowserList getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new BrowserList(context);
+        }
+        return mInstance;
+    }
+
+    public Map<String, Boolean> getBrowserList() {
+        return (Map<String, Boolean>)mStore.getAll();
+    }
+
+    public Map<String, Boolean> getBrowserList(boolean filter) {
+        HashMap<String, Boolean> ret = new HashMap<String, Boolean>();
+        for (Map.Entry<String, Boolean> entry : ((Map<String, Boolean>)mStore.getAll()).entrySet()) {
+            if (entry.getValue() == filter) {
+                ret.put(entry.getKey(), entry.getValue());
+            }
+        }
+        return ret;
+    }
+
+    public void setBrowserEnable(String pkgName, Boolean enable) {
+        mStore.edit().putBoolean(pkgName, enable).commit();
+    }
+
+    public boolean getBrowserEnable(String pkgName) {
+        return mStore.getBoolean(pkgName, false);
+    }
+}
--- app/src/main/java/jp/meridiani/apps/openwithchrome/OpenChromeActivity.java
+++ app/src/main/java/jp/meridiani/apps/openwithchrome/OpenChromeActivity.java
@@ -1,76 +1,83 @@
 package jp.meridiani.apps.openwithchrome;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
-public class OpenChromeActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
 
-    private Uri mUri = null;
-    private final static String mPkgChrome = "com.android.chrome";
+public class OpenChromeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_open_chrome);
-
-        Intent intent = getIntent();
-        if (intent == null) {
-            finish();
-            return;
-        }
-        if (!(Intent.ACTION_SEND.equals(intent.getAction()) &&
-                intent.getType().equals("text/plain") &&
-                intent.getStringExtra(Intent.EXTRA_TEXT) == null)) {
-            finish();
-            return;
-        }
-        try {
-            mUri = Uri.parse(intent.getStringExtra(Intent.EXTRA_TEXT));
-        }
-        catch (Exception e) {
-            finish();
-            return;
-        }
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
 
         try {
-            Intent intent = getPackageManager().getLaunchIntentForPackage(mPkgChrome);
+            Intent intent = getIntent();
             if (intent == null) {
-                throw new PackageManager.NameNotFoundException();
+                return;
             }
-            intent.setAction(Intent.ACTION_VIEW);
-            for ( String cat : intent.getCategories() ) {
-                intent.removeCategory(cat);
+            Bundle extra;
+            if (!(Intent.ACTION_SEND.equals(intent.getAction()) &&
+                    intent.getType().equals("text/plain") &&
+                    (extra = intent.getExtras()) != null)) {
+                return;
             }
-            intent.addCategory(Intent.CATEGORY_DEFAULT);
-            intent.setType("text/plain");
-            intent.setData(mUri);
-            startActivity(intent);
-            finish();
-            return;
-        }
-        catch (PackageManager.NameNotFoundException e) {
-            Toast.makeText(getApplicationContext(),mPkgChrome+" not found.", Toast.LENGTH_LONG);
-            finish();
+            Uri targetUri = Uri.parse(extra.getString(Intent.EXTRA_TEXT));
+
+            ArrayList<Intent> targetIntents = new ArrayList<Intent>();
+
+            PackageManager pm = getPackageManager();
+            Uri dummyUri = Uri.fromParts(targetUri.getScheme(), "", "");
+            Intent dummy = new Intent();
+            dummy.setAction(Intent.ACTION_VIEW);
+            dummy.addCategory(Intent.CATEGORY_DEFAULT);
+            dummy.setType("text/plain");
+            dummy.setData(dummyUri);
+            List<ResolveInfo> rInfos = pm.queryIntentActivities(dummy, 0);
+            for ( ResolveInfo rInfo : rInfos ) {
+                Intent target = new Intent();
+                target.setAction(Intent.ACTION_VIEW);
+                target.setPackage(rInfo.activityInfo.packageName);
+                target.addCategory(Intent.CATEGORY_DEFAULT);
+                target.setType("text/plain");
+                target.setData(targetUri);
+                targetIntents.add(target);
+            }
+            if ( targetIntents.size() < 1 ) {
+                Toast.makeText(getApplicationContext(), "no more browser found", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            Intent chooser = Intent.createChooser(
+                    targetIntents.remove(0),
+                    getString(R.string.select_browser)
+            );
+            if (targetIntents.size() > 0) {
+                chooser.putExtra(
+                        Intent.EXTRA_INITIAL_INTENTS,
+                        targetIntents.toArray(new Parcelable[targetIntents.size()])
+                );
+            }
+
+            startActivity(chooser);
             return;
         }
         catch (Exception e) {
-            Toast.makeText(getApplicationContext(),"can't start "+mPkgChrome, Toast.LENGTH_LONG);
-            finish();
+            Toast.makeText(getApplicationContext(), e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
             return;
         }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
+        finally {
+            finish();
+        }
     }
 }
