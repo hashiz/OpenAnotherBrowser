afb041cdff1cc017cbaa1ed21a7c0cb4862d1b86
hashiz <hashiz@meridiani.jp>
2016-11-29 16:45:08 +0900

    * show Toast when app icon tapped.

--- app/src/main/java/jp/meridiani/apps/openwith/MainActivity.java
+++ app/src/main/java/jp/meridiani/apps/openwith/MainActivity.java
@@ -3,12 +3,14 @@ package jp.meridiani.apps.openwith;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toast.makeText(getApplicationContext(),getString(R.string.this_app_found_in_appshare), Toast.LENGTH_LONG).show();
         finish();
     }
 }
